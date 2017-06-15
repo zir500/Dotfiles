177febff824083afd35723f8f257472fbbf1d89a
Zain Rajput <zir500@york.ac.uk>
2017-06-15 02:25:15 +0100

    Moved .vimrc into the .vim folder to remove a link

--- install.sh
+++ install.sh
@@ -9,5 +9,4 @@ ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
 ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
 ln -sfv "$DOTFILES_DIR/bash/.bashrc" ~
 ln -sfv "$DOTFILES_DIR/vim/.vim" ~
-ln -sfv "$DOTFILES_DIR/vim/.vimrc" ~
 ln -sfv "$DOTFILES_DIR/tmux/.tmux.conf" ~
